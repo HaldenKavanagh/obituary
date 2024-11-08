9d434206fecf12d44c5fc14201d6a4df69a5e6e1
Halden Kavanagh <haldenk@razoyo.com>
2024-11-07 22:53:25 -0700

    added npm, tailwind css imported via npm

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1d4ed8',
+        secondary: '#9333ea',
+        accent: '#14b8a6',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
