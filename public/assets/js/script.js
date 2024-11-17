952f46f337fbc3a0db6beacd6a7a0ecffe089e8e
Halden Kavanagh <haldenk@razoyo.com>
2024-11-16 21:22:13 -0700

    set up pages, html and styling

---
+++ public/assets/js/script.js
@@ -0,0 +1,20 @@
+console.log("hello from script.js");
+
+// Initialize Email.js with your user ID
+emailjs.init({
+  publicKey: "FuUQqDZffoBgp7Xd2",
+});
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    emailjs.sendForm("service_eahlnl5", "template_e2uplpe", this).then(
+      () => {
+        console.log("SUCCESS!");
+      },
+      (error) => {
+        console.log("FAILED...", error);
+      }
+    );
+  });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: ["./public/**/*.html"],
   theme: {
     extend: {
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+      },
       colors: {
-        beige: "#f5e5c1",
+        beige: "#f5ebe0",
         brown: "#4a4437",
         accent: "#14b8a6",
-      },
-      fontFamily: {
-        sans: ["Inter", "sans-serif"],
       },
     },
   },
